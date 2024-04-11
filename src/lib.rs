c0b89b7136fcb95836b07acd87d59e492d94c12f
JoeCleveland <33639708+JoeCleveland@users.noreply.github.com>
2024-04-11 15:08:03 -0400

    Added multiply function

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn mul(left: usize, right: usize) -> usize {
+    left * right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +14,8 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+
+        let result = mul(2, 3);
+        assert_eq!(result, 6);
     }
 }
